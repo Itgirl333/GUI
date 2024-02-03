8d801b63f780e1c7f9b9cda54f74afa9b14b9f34
Crystal <lingchequ@gmail.com>
2024-02-03 12:53:38 +0000

    modify

--- app.py
+++ app.py
@@ -9,18 +9,18 @@ load_dotenv()
 BLYNK_TOKEN = os.environ.get('BLYNK_TOKEN')
 url = f"https://blynk.cloud/external/api/get?token={BLYNK_TOKEN}&v0&v1"
 
-print(url)
+st_autorefresh(interval=5000)
+st.title('Pico_W 專案')
 
 res = requests.get(url)
-if res.ok:
+if res.status_code == 200:
     result = res.json()
     light = result['v0']
     vr = result['v1']
 
-    st.title('Pico_W 專案')
-    st.header(f':red[光線]: {light}  :blue[溫度]: {vr}')
-
     st.header('光線:')
-
+    st.info(light)
     st.header('溫度:')
-
+    st.info(vr)
+else:
+    st.write("連線失敗,請等一下再試")
